408a73f9c4ac574036bc41b9b08720da8b2b36c8
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2018-02-21 11:46:23 -0300

    Update DataTablesHelper.php

--- src/View/Helper/DataTablesHelper.php
+++ src/View/Helper/DataTablesHelper.php
@@ -17,20 +17,19 @@ use Cake\View\Helper;
 class DataTablesHelper extends Helper
 {
 
-    public $helpers     = ['Html'];
+    public $helpers = ['Html'];
     public $wasRendered = [];
-    public $json        = [];
+    public $json = [];
 
     public function initialize(array $config)
     {
         parent::initialize($config);
         $this->json['data'] = [];
     }
-    
+
     public function render($name, array $options = [])
     {
-        if (empty($this->_View->viewVars["DataTables"][$name]))
-        {
+        if (empty($this->_View->viewVars["DataTables"][$name])) {
             throw new \Cake\Error\FatalErrorException(__d('datatables', 'The requested DataTables config was not configured or set to view in controller'));
         }
         $config = $this->_View->viewVars["DataTables"][$name];
@@ -40,38 +39,30 @@ class DataTablesHelper extends Helper
         $options['id'] = $config['id'];
 
         $options += [
-            'width'       => '100%',
+            'width' => '100%',
             'cellspacing' => 0,
-            'class'       => 'display'
+            'class' => 'display'
         ];
 
-
-        foreach ($config['columns'] as $item)
-        {
+        foreach ($config['columns'] as $item) {
             $cols[] = $item['label'];
         }
 
-
         $html = $this->Html->tag('table', $this->Html->tag('thead', $this->Html->tableHeaders($cols)), $options);
-
 
         return $html;
     }
 
     public function prepareData(array $data)
     {
-
         $this->json['data'][] = $data;
     }
 
     public function response()
     {
-
         $data = $this->json['data'];
-        
-        
 
-        $this->json         = $this->_View->viewVars['resultInfo'];
+        $this->json = $this->_View->viewVars['resultInfo'];
         $this->json['data'] = $data;
 
         echo json_encode($this->json, JSON_PRETTY_PRINT);
@@ -80,78 +71,61 @@ class DataTablesHelper extends Helper
     public function setJs()
     {
 
-        if (!empty($this->_View->viewVars["DataTables"]))
-        {
+        if (!empty($this->_View->viewVars["DataTables"])) {
             $html = "<script>$(document).ready(function() {";
-            foreach ($this->wasRendered as $item)
-            {
+            foreach ($this->wasRendered as $item) {
 
                 $config = $this->_View->viewVars["DataTables"][$item];
 
-
-
-                if (!empty($config['options']))
-                {
+                if (!empty($config['options'])) {
                     $options = $config['options'];
-                } else
-                {
+                } else {
                     $options = [];
                 }
 
                 $columnCount = 0;
-                $order       = [];
-                $columnDefs  = [];
-                foreach ($config['columns'] as $column)
-                {
-                    if (!empty($column['order']))
-                    {
+                $order = [];
+                $columnDefs = [];
+                foreach ($config['columns'] as $column) {
+                    if (!empty($column['order'])) {
                         $order[] = [$columnCount, $column['order']];
                     }
 
-
-
                     $columnDefs[] = [
-                        'targets'        => $columnCount,
-                        'searchable'     => $column['searchable'],
-                        'orderable'      => $column['orderable'],
-                        'className'      => $column['className'],
-                        'name'           => $column['name'],
-                        'orderDataType'  => $column['orderDataType'],
-                        'type'           => $column['type'],
-                        'title'          => $column['label'],
-                        'visible'        => $column['visible'],
-                        'width'          => $column['width'],
+                        'targets' => $columnCount,
+                        'searchable' => $column['searchable'],
+                        'orderable' => $column['orderable'],
+                        'className' => $column['className'],
+                        'name' => $column['name'],
+                        'orderDataType' => $column['orderDataType'],
+                        'type' => $column['type'],
+                        'title' => $column['label'],
+                        'visible' => $column['visible'],
+                        'width' => $column['width'],
                         'defaultContent' => $column['defaultContent'],
                         'contentPadding' => $column['contentPadding'],
-                        'cellType'       => $column['cellType']
+                        'cellType' => $column['cellType']
                     ];
 
                     $columnCount++;
                 }
 
-
-
                 $options['processing'] = true;
                 $options['serverSide'] = true;
-                $options['ajax']       = \Cake\Routing\Router::url(['controller' => $this->request->params['controller'], 'action' => 'getDataTablesContent', $item]);
-                $options['order']      = $order;
+                $options['ajax']['url'] = \Cake\Routing\Router::url(['controller' => $this->request->params['controller'], 'action' => 'getDataTablesContent', $item]);
+                if (!empty($options['ajax']['error'])) {
+                    $options['ajax']['error'] = "%f%function(xhr, error, thrown){{$options['ajax']['error']}}%f%";
+                }
+                $options['order'] = $order;
                 $options['columnDefs'] = $columnDefs;
 
-
-
                 $html .= "$('#" . $config['id'] . "').DataTable(";
-
-
                 $html .= json_encode($options);
-
-
-
+                $html = str_replace(['"%f%', '%f%"'], "", $html);
                 $html .= ");";
             }
             $html .= '} );</script>';
-
-
-
+            
             return $html;
         }
     }
