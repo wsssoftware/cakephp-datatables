49960ac2383282a8945831bc6fc45463157fac34
Rachman Chavik <rchavik@xintesa.com>
2018-03-06 11:08:20 +0700

    Add support for friendsofcake/search

---
+++ src/Controller/FocSearchRequestTrait.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace DataTables\Controller;
+
+use Cake\Error\FatalErrorException;
+use \Cake\Utility\Inflector;
+
+/**
+ * CakePHP DataTablesComponent
+ *
+ * @property \DataTables\Controller\Component\DataTablesComponent $DataTables
+ *
+ * @author allan
+ */
+trait FocSearchRequestTrait
+{
+
+    /**
+     * @var callable
+     */
+    private $dataTableBeforeAjaxFunction = null;
+
+    /**
+     * @var callable
+     */
+    private $dataTableAfterAjaxFunction = null;
+
+    /**
+     * Set a function to be exec before ajax request
+     * @param callable $dataTableBeforeAjaxFunction
+     */
+    public function setDataTableBeforeAjaxFunction(callable $dataTableBeforeAjaxFunction)
+    {
+        if (!is_callable($dataTableBeforeAjaxFunction)) {
+            throw new FatalErrorException(__d("datatables", "the parameter must be a function"));
+        }
+        $this->dataTableBeforeAjaxFunction = $dataTableBeforeAjaxFunction;
+    }
+
+    /**
+     * Set a function to be exec after ajax request
+     * @param callable $dataTableAfterAjaxFunction
+     */
+    public function setDataTableAfterAjaxFunction(callable $dataTableAfterAjaxFunction)
+    {
+        if (!is_callable($dataTableAfterAjaxFunction)) {
+            throw new FatalErrorException(__d("datatables", "the parameter must be a function"));
+        }
+        $this->dataTableAfterAjaxFunction = $dataTableAfterAjaxFunction;
+    }
+
+    /**
+     * Ajax method to get data dynamically to the DataTables
+     * @param string $config
+     */
+    public function getDataTablesContent($config)
+    {
+        if (!empty($this->dataTableBeforeAjaxFunction) and is_callable($this->dataTableBeforeAjaxFunction)) {
+            call_user_func($this->dataTableBeforeAjaxFunction);
+        }
+
+        $this->request->allowMethod('ajax');
+        $configName = $config;
+        $config = $this->DataTables->getDataTableConfig($configName);
+        $params = $this->request->query;
+        $this->viewBuilder()->className('DataTables.DataTables');
+        $this->viewBuilder()->template(Inflector::underscore($configName));
+
+        $order = [];
+        if (!empty($params['order'])) {
+            foreach ($params['order'] as $item) {
+                $order[$config['columnsIndex'][$item['column']]] = $item['dir'];
+            }
+        }
+
+        $Table = $this->{$config['table']};
+
+        $results = $Table
+            ->find('search', [
+                'search' => $this->request->query,
+            ]);
+
+        if ($config['finder'] != 'all') {
+            $results->find($config['finder']);
+        }
+
+        if (!empty($params['search']['value'])) {
+            $Schema = $Table->getSchema();
+            if ($displayField = $Table->displayField()) {
+                if ($Schema->getColumnType($displayField) == 'string') {
+                    $cond = [
+                        "$displayField like" => "%{$params['search']['value']}%",
+                    ];
+                } else {
+                    $cond = [
+                        "$displayField" => "{$params['search']['value']}",
+                    ];
+                }
+                $results->where($cond);
+            } else {
+                throw new \UnexpectedValueException("displayField not defined for " . $config['table']);
+            }
+        }
+
+        $countQuery = clone($results);
+
+        $results
+            ->limit($params['length'])
+            ->offset($params['start'])
+            ->order($order);
+
+        $resultInfo = [
+            'draw' => (int)$params['draw'],
+            'recordsTotal' => (int)$countQuery->count(),
+            'recordsFiltered' => (int)$results->count()
+        ];
+
+        $this->set([
+            'results' => $results,
+            'resultInfo' => $resultInfo,
+        ]);
+
+        if (!empty($this->dataTableAfterAjaxFunction) and is_callable($this->dataTableAfterAjaxFunction)) {
+            call_user_func($this->dataTableAfterAjaxFunction);
+        }
+    }
+
+}
