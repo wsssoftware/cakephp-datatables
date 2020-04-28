6e323b1b2f8cfaeeddef06cd466a597136a7d6c5
Allan Carvalho <allan.m.carvalho@outlook.com>
2020-04-28 16:00:37 -0300

    feat: Working on the new version

--- src/Command/DataTablesCommand.php
+++ src/Command/DataTablesCommand.php
@@ -11,9 +11,11 @@
 namespace DataTables\Command;
 
 use Bake\Command\SimpleBakeCommand;
+use Bake\Utility\TableScanner;
 use Cake\Console\Arguments;
 use Cake\Console\ConsoleIo;
 use Cake\Console\ConsoleOptionParser;
+use Cake\Datasource\ConnectionManager;
 use Cake\ORM\Table;
 use Cake\Utility\Inflector;
 
@@ -67,31 +69,87 @@ class DataTablesCommand extends SimpleBakeCommand {
 	 * @inheritDoc
 	 */
 	public function execute(Arguments $args, ConsoleIo $io): ?int {
-		$table = $args->getOption('table');
-		if (empty($table)) {
-			$table = $args->getArgument('name');
+		if ($args->getArgument('type') === 'config') {
+			$this->connection = $args->getOption('connection');
+			/** @var \Cake\Database\Connection $connection */
+			$connection = ConnectionManager::get($this->connection);
+			$scanner = new TableScanner($connection);
+			$table = $args->getOption('table');
+			if (empty($table)) {
+				$table = $args->getArgument('name');
+			}
+			$table = Inflector::tableize($table);
+			if (!in_array($table, $scanner->listUnskipped())) {
+				$io->warning(sprintf("You tried to use a database table '%s' that doesn't exist in the database.", Inflector::camelize($table)));
+				$io->info('Create the database table if not exists or use one of options bellow:');
+				foreach ($scanner->listUnskipped() as $item) {
+					$io->out('- ' . Inflector::camelize($item));
+				}
+				return static::CODE_SUCCESS;
+			}
+		} else {
+			$callback = $args->getArgument('name');
+			$validCallbacks = $this->getValidCallbacks();
+			if (!in_array($callback, $validCallbacks)) {
+				$io->warning(sprintf("You tried to use a callback '%s' that doesn't exist in plugin.", $callback));
+				$io->info('Use one of options bellow:');
+				foreach ($validCallbacks as $item) {
+					$io->out('- ' . Inflector::camelize($item));
+				}
+				return static::CODE_SUCCESS;
+			}
+
 		}
-		$table = Inflector::camelize($table);
+
 		$tableObject = $this->getTableLocator()->get($table);
 		if (get_class($tableObject) === Table::class) {
 			if (empty($args->getOption('table'))) {
 				$io->warning("The name '$table' used for the DataTables class does not belong to any database table.");
 				$io->info('If this name is correct, use the option \'--table=Foo\' to enter the name of the database table that you want to use in this class.');
 			} else {
-				$io->warning('You tried to use a database table that doesn\'t exist in the database or just hasn\'t yet been baked in your app.');
-				$io->info('Create the database table if not exists and/or bake the model before try bake the DataTables class again.');
+
 			}
-			return static::CODE_SUCCESS;
 		}
 		$this->_table = $tableObject;
 		return parent::execute($args, $io);
 	}
 
 	/**
+	 * @return array
+	 */
+	private function getValidCallbacks(): array {
+		$dir = DATA_TABLES_ROOT . DS . 'templates' . DS . 'twig' . DS . 'js' . DS . 'bake' . DS;
+		$callbacks = scandir($dir);
+		foreach ($callbacks as $index => $callback) {
+			if (in_array($callback, ['.', '..'])) {
+				unset($callbacks[$index]);
+			} else {
+				$callbacks[$index] = str_replace(['callback_', '.twig'], '', $callback);
+			}
+		}
+		return $callbacks;
+	}
+
+	/**
 	 * @inheritDoc
 	 */
 	public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser {
-		$parser = parent::buildOptionParser($parser);
+		$parser = $this->_setCommonOptions($parser);
+		$parser->setDescription(
+			sprintf('Bake the plugin DataTables necessaries files.')
+		)->addArgument('type', [
+			'help' => sprintf(
+				"A type of bake that you want to do. Must be 'table' or 'callback'"
+			),
+			'choices' => ['config', 'callback'],
+			'required' => true,
+		])->addArgument('name', [
+			'help' => sprintf(
+				'The name of config/callback that will baked.'
+			),
+			'required' => true,
+		]);
+
 		$parser->addOption('table', [
 			'short' => 't',
 			'help' => 'Database to use in DataTables class.',
