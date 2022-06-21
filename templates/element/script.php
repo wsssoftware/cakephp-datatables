6a8687c6fdfd2644926be81cd8f931b3f8c74b3a
nshiddqui <49165091+nshiddqui@users.noreply.github.com>
2022-06-21 16:00:47 +0530

    Update script.php

--- templates/element/script.php
+++ templates/element/script.php
@@ -21,7 +21,7 @@ foreach ($configBundles as $configBundle) {
 	$body .= "            $('#{$configBundle->getUniqueId()}').DataTable($bodyJson);\n";
     $currentPage = $configBundle->Options->getCurrentPage();
 	if ($currentPage !== null) {
-        $body .= "            $('#{$configBundle->getUniqueId()}').on('init.dt', function ( e, settings, json ) {\n";
+        $body .= "window[{$configBundle->getUniqueId()}] = $('#{$configBundle->getUniqueId()}').on('init.dt', function ( e, settings, json ) {\n";
         $body .= "                setTimeout( function () {\n";
         $body .= "                    (new $.fn.dataTable.Api(settings)).page($currentPage).draw(false);\n";
         $body .= "                }, 5 );\n";
